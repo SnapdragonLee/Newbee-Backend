28dcbc4e99d0dbadf3d148d87c47438880672361
JTL <1483870315@qq.com>
2022-04-14 14:15:02 +0800

    fix admin_login bug

--- src/mysite/utils/response.py
+++ src/mysite/utils/response.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 
 def drf_response(ret, **data):
     if ret == 0:
-        data['ret'] = '0'
+        data['ret'] = 0
         return Response(data=data, status=200)
     elif ret == 1:
-        return Response(data={"ret": '1'}, status=200)
+        return Response(data={"ret": 1}, status=200)
     elif ret == 2:
-        return Response(data={"ret": '2'}, status=200)
+        return Response(data={"ret": 2}, status=200)

56f30a162a0148181686f44dce16c1e48436025c
SnapdragonLee <lixiaolong8088888@gmail.com>
2022-04-19 05:28:55 +0800

    change to script instead of transfer every version of dist/

--- src/mysite/dist/static/js/7.d1fe57c9b5f2b06d8255.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([7],{B3xJ:function(e,t){},WOUj:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("router-view")],1)},staticRenderFns:[]};var i=n("VU/8")({name:"Questions"},r,!1,function(e){n("B3xJ")},"data-v-164f931d",null);t.default=i.exports}});
-//# sourceMappingURL=7.d1fe57c9b5f2b06d8255.js.map

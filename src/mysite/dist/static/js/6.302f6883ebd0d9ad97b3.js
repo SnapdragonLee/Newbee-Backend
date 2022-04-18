8284e565833e69c6c5620c444bca6edee5b58ae4
SnapdragonLee <lixiaolong8088888@gmail.com>
2022-04-19 02:40:51 +0800

    run new frontend

--- src/mysite/dist/static/js/6.302f6883ebd0d9ad97b3.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([6],{B3xJ:function(e,t){},WOUj:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("router-view")],1)},staticRenderFns:[]};var i=n("VU/8")({name:"Questions"},r,!1,function(e){n("B3xJ")},"data-v-164f931d",null);t.default=i.exports}});
-//# sourceMappingURL=6.302f6883ebd0d9ad97b3.js.map

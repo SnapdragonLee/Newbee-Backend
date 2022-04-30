69ef9430511ce6e4d5026c52b2645e2ba18c978e
JTL <1483870315@qq.com>
2022-04-30 21:56:32 +0800

    加了小程序端的查看修改用户名

--- src/mysite/client/urls.py
+++ src/mysite/client/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.user_login, name='user_login'),
+    path('profile', views.UserProfile.as_view()),
     path('solution', views.SolutionViewClass.as_view()),
     path('solution_like', views.solution_like),
     path('solution_report', views.solution_report)
--- src/mysite/client/views.py
+++ src/mysite/client/views.py
@@ -9,14 +9,21 @@ from django.http import JsonResponse
 from django.views.generic.base import View
 from django.utils.decorators import method_decorator
 from utils.auth_decorators import user_logged
+from django.db import transaction
 
 
 # Create your views here.
 def user_login(request):
     appid = 'wx9cb76a70a7aba68a'
     secret = 'a79c0044ff02e5368cf3bc96704f7d76'
-    post_data = json.loads(request.body)
-    code = post_data['code']
+
+    try:
+        post_data = json.loads(request.body)
+        code = post_data['code']
+        wx_username = post_data['username']
+    except Exception as e:
+        print(e.args)
+        return JsonResponse(data=wrap_response_data(3, "json格式错误"))
 
     url = 'https://api.weixin.qq.com/sns/jscode2session' + '?appid=' + appid \
           + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code'
@@ -28,10 +35,15 @@ def user_login(request):
     openid = response['openid']
     session_key = response['session_key']
 
-    user = WXUser.objects.filter(id=openid)
-    if not user:
-        new_user = WXUser(id=openid, user_name='默认用户名')
-        new_user.save()
+    try:
+        with transaction.atomic():
+            user = WXUser.objects.filter(id=openid)
+            if not user:
+                new_user = WXUser(id=openid, user_name=wx_username)
+                new_user.save()
+    except Exception as e:
+        print(e.args)
+        return JsonResponse(data=wrap_response_data(3, "数据库操作失败"))
 
     request.session['openid'] = openid
     return JsonResponse(data=wrap_response_data(0))
@@ -42,6 +54,45 @@ def user_login(request):
     #
     # return Response(data={'ret': 0, 'key': key})
     pass
+
+
+class UserProfile(View):
+    @method_decorator(user_logged)
+    def get(self, request):
+        open_id = request.session['openid']
+
+        data = {}
+        try:
+            with transaction.atomic():
+                user = WXUser.objects.get(id=open_id)
+                data['username'] = user.user_name
+        except Exception as e:
+            print(e.args)
+            return JsonResponse(data=wrap_response_data(3, "不存在为该openid的用户"))
+
+        return JsonResponse(data=wrap_response_data(0, **data))
+
+    @method_decorator(user_logged)
+    def post(self, request):
+        open_id = request.session['openid']
+
+        try:
+            post_data = json.loads(request.body)
+            new_username = post_data['username']
+        except Exception as e:
+            print(e.args)
+            return JsonResponse(data=wrap_response_data(3, "json格式错误"))
+
+        try:
+            with transaction.atomic():
+                user = WXUser.objects.get(id=open_id)
+                user.user_name = new_username
+                user.save()
+        except Exception as e:
+            print(e.args)
+            return JsonResponse(data=wrap_response_data(3, "不存在为该openid的用户"))
+
+        return JsonResponse(data=wrap_response_data(0))
 
 
 class SolutionViewClass(View):
@@ -61,7 +112,7 @@ def solution_like(request):
         solution_id = post_data['solution_id']
     except Exception as e:
         print(e.args)
-        return JsonResponse(data=wrap_response_data(3, '参数格式不符'))
+        return JsonResponse(data=wrap_response_data(3, 'json格式不符'))
 
     # todo 同一个用户对同一个题解只能点赞或举报一次
 
